89df78622cc2c567217be6464600579cce60bf7e
Marcos Motta <mrcsmotta1@gmail.com>
2020-08-11 19:11:04 -0300

    Criando metodo para selecionar Users

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function relBooks()
+    {
+        return $this->hasMany('App\Models\ModelBook', 'id_user');
+    }
 }
